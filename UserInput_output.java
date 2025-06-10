9339ab13d292d98f6d5e14cd81f2449a94d0682f
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-06-10 22:30:53 +0530

    first

---
+++ UserInput_output.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserInput_output {
+    public static void printNumber(Scanner input) {
+        System.out.println("Enter a number:");
+        int num = input.nextInt();
+        System.out.println(num);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        printNumber(scanner);
+    }
+}
