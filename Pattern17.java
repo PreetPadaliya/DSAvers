b65257c0f4b9e965609b8ece7fee5a59ff248187
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-06-16 17:36:46 +0530

    Patterns 75%

---
+++ Pattern17.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+
+        System.out.print("Enter a number : ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2*i - 1; k++){
+                System.out.print((char)('A'+k));
+            }
+            System.out.println();
+        }
+    }
+}
