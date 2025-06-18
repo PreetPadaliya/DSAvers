24d825352ab3b6bb22e2e4bdde7af9974678199e
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-06-18 18:50:09 +0530

    Nothing

--- Pattern19.java
+++ Pattern19.java
@@ -5,9 +5,13 @@ public class Pattern19 {
 
         System.out.print("Enter a number : ");
         Scanner input = new Scanner(System.in);
-        int num = input.nextInt();
+        int n = input.nextInt();
 
-
-
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j<=i ; j++){
+                System.out.print(j);
+            }
+        }
+        System.out.println();
     }
 }
