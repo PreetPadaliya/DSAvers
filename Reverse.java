233edd15838bf56070ae82ce13646ecf42de8559
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-11-26 23:06:29 +0530

    checking

--- Reverse.java
+++ Reverse.java
@@ -11,7 +11,7 @@ public class Reverse {
         while (n>0){
             int lastDigit = n%10;
             rev = (rev*10)+lastDigit;
-            n = n/10;
+            n = n/10;       
         }
         System.out.println(rev);
     }
